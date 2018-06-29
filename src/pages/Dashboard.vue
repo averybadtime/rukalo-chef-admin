acd309380eb5f93690e003a31cf3a1f120ca2ca4
Jean Carlos Ospino González <devjc23@gmail.com>
2018-06-29 15:02:37 -0500

    Arreglado el bug de la navegación en la vista movil

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Dashboard</h1>
+  <router-view></router-view>
 </template>
+
+<script>
+  import DashboardPartial from "@/partials/Dashboard"
+  export default {
+    components: {
+      DashboardPartial
+    },
+    mounted() {
+      toggleDashboardResponsive()
+    }
+  }
+</script>
--- src/scripts/rukalo.js
+++ src/scripts/rukalo.js
@@ -2,3 +2,14 @@
 function toggleDashboardResponsive () {
   $('.dashboard-responsive-nav-trigger').on('click',function(e){e.preventDefault();$(this).toggleClass('active');var dashboardNavContainer=$('body').find(".dashboard-nav");if($(this).hasClass('active')){$(dashboardNavContainer).addClass('active');}else{$(dashboardNavContainer).removeClass('active');}});
 }
+
+function toogleDashboardHeight() {
+  var winWidth = $(window).width();
+  var headerHeight = $("#header-container").height();
+  $("#dashboard").css("padding-top", headerHeight);
+  if (winWidth < 992) {
+      $(".fs-inner-container.map-fixed").insertBefore(".fs-inner-container.content");
+  } else {
+      $(".fs-inner-container.content").insertBefore(".fs-inner-container.map-fixed");
+  }
+}
