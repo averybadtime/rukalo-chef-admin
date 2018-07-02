e89b2bf400832278e935d530b75d03b1c062b1b6
Jean Carlos Ospino González <devjc23@gmail.com>
2018-07-01 22:00:11 -0500

    Añadida la autenticación con Firebase.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { AUTH, DB } from "./firebase"
 import App from './App.vue'
 
 import { routes } from "./routes"
+import { auth } from 'firebase';
 
 Vue.use(VueRouter)
 
@@ -21,23 +22,33 @@ router.beforeEach((to, from, next) => {
   else next() 
 })
 
-function getUserInfo(uid) {
+let app, onValue
+
+function loadAuthUserInfo (uid) {
   return new Promise(resolve => {
-    if (!uid) return resolve(null)
-    store.state.authOnValue = DB.ref("/admin/" + uid).on("value", snapshot => {
-      store.state.user = snapshot.profile
-      return resolve(snapshot.val())
+    if (!uid) return resolve()
+
+    const adminRef = DB.ref("/admin/" + uid)
+    onValue = adminRef.child("profileInfo").on("value", snapshot => {
+      if (snapshot.exists()) return resolve(snapshot.val())
+      else return resolve(null)
     })
   })
 }
 
-let app
-
 AUTH.onAuthStateChanged(user => {
   const uid = user ? user.uid : null
-  getUserInfo(uid).then(userData => {
-    if (userData) {
-      store.state.user = userData
+  loadAuthUserInfo(uid).then(data => {
+    if (data) {
+      if (data.role === "ADMIN") {
+        store.state.firebaseUser = user
+        store.state.user = data
+      } else {
+        AUTH.signOut().then(() => console.log("Cerrando sesión..."))
+        DB.ref("/admin/" + uid + "/profileInfo").off("value", onValue)
+      }
+    } else {
+      if (uid) DB.ref("/admin/" + uid + "/profileInfo").off("value", onValue)
     }
     if (!app) {
       new Vue({
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,3 +1,68 @@
 <template>
-  <h1>Login</h1>
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="container">
+        <h1>Login</h1>
+        <form @submit.prevent="logIn">
+          <p>
+            <label for="email">E-m@il</label>
+            <input type="email" name="email" v-model="auth.email">
+          </p>
+          <p>
+            <label for="password">Contraseña</label>
+            <input type="password" name="password" v-model="auth.password">
+          </p>
+          <p>
+            <input type="submit" class="btn" value="Iniciar sesión">
+          </p>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+  import hasEmptyFields from "@/mixins/hasEmptyFields"
+  import { AUTH, DB } from "@/firebase"
+  export default {
+    mixins: [hasEmptyFields],
+    data() {
+      return {
+        auth: {
+          email: "",
+          password: ""
+        }
+      }
+    },
+    methods: {
+      isAdmin(email) {
+        return new Promise(resolve => {
+          const adminRef = DB.ref("/admin")
+          adminRef.orderByChild("profileInfo/email").equalTo(email).once("value", snapshot => {
+            return resolve(snapshot.exists())
+          })
+        })
+      },
+      logIn() {
+        if (this.hasEmptyFields(this.auth)) {
+          const message = "No has rellenado todos los campos."
+					console.error(message)
+					alert(message)
+					return
+        }
+        const email = this.auth.email
+        const password = this.auth.password
+        this.isAdmin(email).then(isAdmin => {
+          if (isAdmin) {
+            AUTH.signInWithEmailAndPassword(this.email, this.password).then(() => this.$router.replace("/dashboard"))
+            .catch(err => alert(err))
+          } else {
+            const message = "Este usuario no se encuentra registrado."
+            console.error(message)
+            alert(message)
+          }
+        })
+      }
+    }
+  }
+</script>
